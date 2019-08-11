5cd917353c790fbbb6dc5bcce650b83c962592f7
Luke Joliat <luke.joliat@S02079-MBPR.local>
2019-08-11 13:59:21 -0400

    initial commit by me

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome!</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload!
         </p>
         <a
           className="App-link"
